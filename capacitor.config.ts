0ec10e34fb0747ed4c389b0c59b7d53299dda44b
Dave <dave@webdave.de>
2023-11-08 15:47:57 +0100

    capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,12 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.nashville',
+  appId: 'com.webdave.nashville',
   appName: 'nashville',
-  webDir: 'dist',
+  webDir: 'dist/nashville/browser',
   server: {
-    androidScheme: 'https'
-  }
+    androidScheme: 'https',
+  },
 };
 
 export default config;
